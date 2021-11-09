f900673c719201e92e751ae8b320b94bcd43bae2
Luciano <oliveiraluck78@gmail.com>
2021-11-09 03:36:25 -0300

    Todos exercicios feitos

---
+++ Fundamentos/Bloco-4/Dia_2_arrya_for/exercicios.js
@@ -0,0 +1,76 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercicio 1:
+// for (let num of numbers) {
+//     console.log(num);
+// }
+
+// Exercicio 2
+// let numbers2 = 0 ;
+// for (let num of numbers) {
+//     numbers2+= num;
+// }
+// console.log(numbers2);
+
+// Exercicio 3
+// let numbers2 = 0 ;
+// for (let num of numbers) {
+//     numbers2+= num;
+// }
+// console.log(numbers2 / numbers.length);
+
+// Exercicio 4
+// let numbers2 = 0 ;
+// for (let num of numbers) {
+//     numbers2+= num;
+// }
+// numbers2 = numbers2 / numbers.length;
+// switch (numbers2) {
+//     case numbers2 < 20:
+//         console.log('numero é maior que 20')
+//         break
+//     default:
+//         console.log('Numero é maior que 20')
+// }
+
+// Exercicio 5
+// let cont = 0;
+// for(let num of numbers) {
+//     if (cont < num) {
+//         cont = num;
+//     }
+// }
+// console.log(cont)
+
+// Exercicio 6
+// let cont = 0;
+// for (let num of numbers) {
+//     if ((num % 2) != 0) {
+//         cont += 1
+//     }
+// }
+// if (cont > 0) {
+//     console.log('Existem ' + cont + ' Numeros impares')
+// }
+// else {
+//     console.log('Nenhum valor encontrado')
+// }
+
+// Exercicio 7
+//  let cont = numbers[0];
+//  for(let num of numbers) {
+//      if (cont > num) {
+//         cont = num;
+//      }
+// }
+//  console.log(cont)
+
+let numbers2 = [];
+for(index = 1; index != 26; index+=1) {
+    numbers2.push(index)
+}
+// console.log(numbers2)
+
+for(let num of numbers2){
+    console.log(num / 2)
+}
