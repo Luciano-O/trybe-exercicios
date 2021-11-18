ea327fe1c10862dbfa50e553eef6e4083e648a4f
Luciano <oliveiraluck78@gmail.com>
2021-11-18 04:44:01 -0300

    Exercicio 3

--- Fundamentos/Bloco_5/Dia 2/script.js
+++ Fundamentos/Bloco_5/Dia 2/script.js
@@ -5,3 +5,7 @@ document.body.appendChild(exe);
 let main = document.createElement('main');
 main.className = 'main-content';
 document.body.appendChild(main);
+
+let secao = document.createElement('section');
+secao.className = 'center-content';
+document.querySelector('.main-content').appendChild(secao);
