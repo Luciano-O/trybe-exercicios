6e92617d4cf7dc6a77358b0df18ca1a354c25d40
Luciano <oliveiraluck78@gmail.com>
2021-12-06 19:15:59 -0300

    Adicionando tudo

--- Fundamentos/Bloco 7/Dia 1/script.js
+++ Fundamentos/Bloco 7/Dia 1/script.js
@@ -1,13 +1,15 @@
-function testingScope(escopo) {
+const testingScope = (escopo) => {
   if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope =  '${ifScope} ótimo, fui utilizada no escopo !';
     console.log(ifScope);
   } else {
-    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  let ifScope = 'To aqui agr';
+  console.log(` o que estou ${ifScope} fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
 }
 
 testingScope(true);
+
