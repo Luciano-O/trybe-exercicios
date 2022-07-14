35ab7666e6ae476d02f61edffd0e768fff092fb2
Luciano <oliveiraluck78@gmail.com>
2022-07-14 15:19:11 -0300

    FINAL COMMIT

--- bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Scholl/src/classes.ts
+++ bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Scholl/src/classes.ts
@@ -1,33 +1,57 @@
-export class student {
-  nome: string
-  private _matricula: string
-  private _notasProvas: [number, number, number, number]
-  private _notasTrabalhos: [number, number]
+import IEmployee from "./interfaces";
 
-  constructor(n: string, m: string, nP: [number, number, number, number], nT: [number, number]) {
-    this.nome = n
-    this._matricula = m
-    this._notasProvas = nP
-    this._notasTrabalhos = nT
+export abstract class Person {
+  constructor (public name: string, private birthDate: Date) {
+    const now = Date.now()
+    const year = birthDate.getFullYear();
+    if (name.length < 3) this.name = 'generic'
+    if (now < this.birthDate.getTime()) this.birthDate = new Date(2022, 0o7, 12)
+    if (year < 1922) this.birthDate = new Date(2022, 0o7, 12)
   }
 
-  sumNotas() {
-    const somaProvas = this._notasProvas.reduce(
+  get birth() {
+    return this.birthDate;
+  }
+}
+
+export class student extends Person {
+  private _enrollment: Date
+  private _examsGrades: [number, number, number, number]
+  private _worksGrades: [number, number]
+
+  constructor(n: string, eG: [number, number, number, number], wG: [number, number], bd: Date) {
+    super(n, bd)
+    this._enrollment = new Date(Date.now())
+    this._examsGrades = eG
+    this._worksGrades = wG
+    this.generateEnrollment()
+
+    if(this._examsGrades.length > 4) this._examsGrades.length = 4
+    if(this._worksGrades.length > 2) this._worksGrades.length = 2
+  }
+
+  generateEnrollment(): void {
+    console.log(`Matricula realizada com sucesso na data: ${this._enrollment}`);
+  }
+
+  sumGrades() {
+    const somaProvas = this._examsGrades.reduce(
       (prev, atual) => prev + atual, 0
     )
-    const somaTrabalhos = this._notasTrabalhos.reduce(
+    const somaTrabalhos = this._worksGrades.reduce(
       (prev, atual) => prev + atual, 0
     )
 
     return somaProvas + somaTrabalhos
   }
 
-  mediaNotas() {
-    const sum = this.sumNotas();
+  sumAverageGrade() {
+    const sum = this.sumGrades();
 
     return sum / 6
   }
 }
+
 
 export class client {
   name: string
@@ -87,5 +111,40 @@ export class Data {
     this.dia = d
     this.mes = m
     this.ano = a
+  }
+}
+
+export class Subject {
+  constructor (private name: string) {
+    if (this.name.length < 3) this.name = 'generic'
+  }
+
+  set setName(n: string) {
+    this.name = n
+  }
+
+  get getName() {
+    return this.name
+  }
+}
+
+export class Teacher extends Person implements IEmployee {
+  admissionDate: Date;
+
+  constructor (
+    name: string,
+    birthDate: Date,
+    public subject: string,
+    public salary: number,
+    public registration: number,
+  ) {
+    super(name, birthDate)
+    const date = new Date(Date.now())
+    this.admissionDate = date
+    this.generateRegistration()
+  }
+
+  generateRegistration() {
+    return(`The registration has been complete at: ${this.admissionDate}`);
   }
 }
